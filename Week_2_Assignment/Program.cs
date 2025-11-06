ebf90bb1b58ddb7453301724fe60205972579daa
manish5601 <manish5601@gmail.com>
2025-11-06 21:12:05 +0545

    Task-3 compleated

--- Week_2_Assignment/Program.cs
+++ Week_2_Assignment/Program.cs
@@ -1,30 +1,32 @@
-﻿using System;
-
-class Circle
-{
-    public const double PI = 3.14;
-
-    // Method to calculate area of the circle
-    public double CalculateArea(double radius)
-    {
-        return PI * radius * radius;
-    }
-
-    // Method to calculate perimeter of the circle
-    public double CalculatePerimeter(double radius)
-    {
-        return 2 * PI * radius;
-    }
-}
-
+﻿
 class Program
 {
     static void Main()
     {
-        Circle circle = new Circle();
+        byte b = 100;
+        short s = -5000;
+        int i = 123456;
+        long l = 9876543210L;
+        float f = 2.718f;
+        double d = 3.1415926535;
+        decimal m = 12345.6789m;
+        char c = 'X';
+        bool flag = false;
 
-        double radius = 5.0;
-        Console.WriteLine($"Area of circle with radius {radius}: {circle.CalculateArea(radius)}");
-        Console.WriteLine($"Perimeter of circle with radius {radius}: {circle.CalculatePerimeter(radius)}");
+        string intToStr = 42.ToString();
+        double strToDouble = double.Parse("3.14");
+
+        Console.WriteLine($"byte:   {b}   (Type: {b.GetType()})");
+        Console.WriteLine($"short:  {s}   (Type: {s.GetType()})");
+        Console.WriteLine($"int:    {i}   (Type: {i.GetType()})");
+        Console.WriteLine($"long:   {l}   (Type: {l.GetType()})");
+        Console.WriteLine($"float:  {f}   (Type: {f.GetType()})");
+        Console.WriteLine($"double: {d}   (Type: {d.GetType()})");
+        Console.WriteLine($"decimal:{m}   (Type: {m.GetType()})");
+        Console.WriteLine($"char:   '{c}' (Type: {c.GetType()})");
+        Console.WriteLine($"bool:   {flag} (Type: {flag.GetType()})");
+        Console.WriteLine();
+        Console.WriteLine($"42 as string: \"{intToStr}\" (Type: {intToStr.GetType()})");
+        Console.WriteLine($"\"3.14\" as double: {strToDouble} (Type: {strToDouble.GetType()})");
     }
 }
