ff39fb2d24cac3a8982cf10615cf132e7ea83d5c
manish5601 <manish5601@gmail.com>
2025-11-06 21:09:41 +0545

    Task-2 Compleated

--- Week_2_Assignment/Program.cs
+++ Week_2_Assignment/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 
+class Circle
+{
+    public const double PI = 3.14;
+
+    // Method to calculate area of the circle
+    public double CalculateArea(double radius)
+    {
+        return PI * radius * radius;
+    }
+
+    // Method to calculate perimeter of the circle
+    public double CalculatePerimeter(double radius)
+    {
+        return 2 * PI * radius;
+    }
+}
+
 class Program
 {
     static void Main()
     {
-        string userName = "Manish Kumar Chaudhary";
-        int luckyNumber = 1008;
+        Circle circle = new Circle();
 
-        Console.WriteLine($"Hello, {userName}! Your lucky number is {luckyNumber}.");
+        double radius = 5.0;
+        Console.WriteLine($"Area of circle with radius {radius}: {circle.CalculateArea(radius)}");
+        Console.WriteLine($"Perimeter of circle with radius {radius}: {circle.CalculatePerimeter(radius)}");
     }
 }
