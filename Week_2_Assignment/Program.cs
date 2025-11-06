f9a26156668f409533e12dafed58df4eef03326b
manish5601 <manish5601@gmail.com>
2025-11-06 21:05:21 +0545

    Task-1 Completed

--- Week_2_Assignment/Program.cs
+++ Week_2_Assignment/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main()
     {
         string userName = "Manish Kumar Chaudhary";
-        int luckyNumber = 108;
+        int luckyNumber = 1008;
 
         Console.WriteLine($"Hello, {userName}! Your lucky number is {luckyNumber}.");
     }
