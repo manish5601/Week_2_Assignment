c7a696750dc4685989e812250eb0633badd18bbc
manish5601 <manish5601@gmail.com>
2025-11-06 21:02:52 +0545

    Add project files.

---
+++ Week_2_Assignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string userName = "Manish Kumar Chaudhary";
+        int luckyNumber = 108;
+
+        Console.WriteLine($"Hello, {userName}! Your lucky number is {luckyNumber}.");
+    }
+}
