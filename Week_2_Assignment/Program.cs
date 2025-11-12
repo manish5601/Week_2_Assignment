17ba7c8ec72c1cec60f28cd8c74a830adbfa8cad
manish5601 <manish5601@gmail.com>
2025-11-12 09:01:44 +0545

    Task-4 Compleated

--- Week_2_Assignment/Program.cs
+++ Week_2_Assignment/Program.cs
@@ -47,56 +47,56 @@
 //}
 
 //Task- 3
-public class Program
-{
-    public static void Main()
-    {
-        byte b = 255;
-        short s = -32768;
-        int i = 2147483647;
-        long l = 9223372036854775807L;
-        float f = 3.402823E+38f;
-        double d = 1.7976931348623157E+308;
-        decimal m = 79228162514264337593543950335m;
-        char c = 'A';
-        bool flag = true;
-
-        string intToString = 42.ToString();
-        double stringToDouble = double.Parse("3.14");
-
-        System.Console.WriteLine($"byte: {b}");
-        System.Console.WriteLine($"short: {s}");
-        System.Console.WriteLine($"int: {i}");
-        System.Console.WriteLine($"long: {l}");
-        System.Console.WriteLine($"float: {f}");
-        System.Console.WriteLine($"double: {d}");
-        System.Console.WriteLine($"decimal: {m}");
-        System.Console.WriteLine($"char: {c}");
-        System.Console.WriteLine($"bool: {flag}");
-        System.Console.WriteLine($"int 42 to string: {intToString}");
-        System.Console.WriteLine($"string \"3.14\" to double: {stringToDouble}");
-    }
-}
-
-//Task-4
 //public class Program
 //{
 //    public static void Main()
 //    {
-//        int[] favoriteNumbers = new int[] { 7, 3, 9, 1, 5 };
+//        byte b = 255;
+//        short s = -32768;
+//        int i = 2147483647;
+//        long l = 9223372036854775807L;
+//        float f = 3.402823E+38f;
+//        double d = 1.7976931348623157E+308;
+//        decimal m = 79228162514264337593543950335m;
+//        char c = 'A';
+//        bool flag = true;
 
-//        System.Array.Sort(favoriteNumbers);
-//        System.Array.Reverse(favoriteNumbers);
+//        string intToString = 42.ToString();
+//        double stringToDouble = double.Parse("3.14");
 
-//        for (int i = 0; i < favoriteNumbers.Length; i++)
-//        {
-//            System.Console.WriteLine(favoriteNumbers[i]);
-//        }
-
-//        int position = System.Array.IndexOf(favoriteNumbers, 3);
-//        System.Console.WriteLine($"Position of 3: {position}");
+//        System.Console.WriteLine($"byte: {b}");
+//        System.Console.WriteLine($"short: {s}");
+//        System.Console.WriteLine($"int: {i}");
+//        System.Console.WriteLine($"long: {l}");
+//        System.Console.WriteLine($"float: {f}");
+//        System.Console.WriteLine($"double: {d}");
+//        System.Console.WriteLine($"decimal: {m}");
+//        System.Console.WriteLine($"char: {c}");
+//        System.Console.WriteLine($"bool: {flag}");
+//        System.Console.WriteLine($"int 42 to string: {intToString}");
+//        System.Console.WriteLine($"string \"3.14\" to double: {stringToDouble}");
 //    }
 //}
+
+//Task-4
+public class Program
+{
+    public static void Main()
+    {
+        int[] favoriteNumbers = new int[] { 7, 3, 9, 1, 5 };
+
+        System.Array.Sort(favoriteNumbers);
+        System.Array.Reverse(favoriteNumbers);
+
+        for (int i = 0; i < favoriteNumbers.Length; i++)
+        {
+            System.Console.WriteLine(favoriteNumbers[i]);
+        }
+
+        int position = System.Array.IndexOf(favoriteNumbers, 3);
+        System.Console.WriteLine($"Position of 3: {position}");
+    }
+}
 
 //Task-5
 //public class Program
