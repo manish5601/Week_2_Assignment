740fbc42e4b7a20ca66be5c55f2a1186b2809180
manish5601 <manish5601@gmail.com>
2025-11-12 08:43:41 +0545

    Task-1 Commited

--- Week_2_Assignment/Program.cs
+++ Week_2_Assignment/Program.cs
@@ -2,31 +2,40 @@
 {
     static void Main()
     {
-        List<string> fruits = new List<string> { "Apple", "Banana", "Mango" };
+        //Task-6
 
-        fruits.Add("Orange");
+        //List<string> fruits = new List<string> { "Apple", "Banana", "Mango" };
 
-        fruits.Remove("Banana");
+        //fruits.Add("Orange");
 
-        Console.WriteLine("Fruits in List:");
-        foreach (string fruit in fruits)
+        //fruits.Remove("Banana");
+
+        //Console.WriteLine("Fruits in List:");
+        //foreach (string fruit in fruits)
+        //{
+        //    Console.WriteLine(fruit);
+        //}
+
+        //Dictionary<int, string> fruitDict = new Dictionary<int, string>
+        //{
+        //    { 1, "Apple" },
+        //    { 2, "Mango" },
+        //    { 3, "Orange" }
+        //};
+
+        //fruitDict.Add(4, "Grape");
+
+        //Console.WriteLine("\nFruit Dictionary:");
+        //foreach (var pair in fruitDict)
+        //{
+        //    Console.WriteLine($"ID: {pair.Key}, Name: {pair.Value}");
+        //}
+
+        //task 1
         {
-            Console.WriteLine(fruit);
-        }
-
-        Dictionary<int, string> fruitDict = new Dictionary<int, string>
-        {
-            { 1, "Apple" },
-            { 2, "Mango" },
-            { 3, "Orange" }
-        };
-
-        fruitDict.Add(4, "Grape");
-
-        Console.WriteLine("\nFruit Dictionary:");
-        foreach (var pair in fruitDict)
-        {
-            Console.WriteLine($"ID: {pair.Key}, Name: {pair.Value}");
+            string userName = "Alex";
+            int luckyNumber = 108;
+            System.Console.WriteLine($"Hello, {userName}! Your lucky number is {luckyNumber}.");
         }
     }
 }
